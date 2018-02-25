1282a142992bac6d930f7e702d5b6ca952947f00
Rizki Ardian <radeite8@gmail.com>
2018-02-25 14:39:41 +0700

    Initial Commit

---
+++ tobedone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tobedone
+//
+//  Created by Rizki Ardian on 25/2/2018.
+//  Copyright © 2018 Rizki Ardian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
